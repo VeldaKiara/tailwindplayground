cb5d9ba628d1331c648b63c5e55e8bb8ea391700
Velda Kiara <32552296+veldakarimi@users.noreply.github.com>
2021-02-16 13:58:59 +0300

    Js file with components from tailwind

---
+++ config.css
@@ -0,0 +1,14 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
